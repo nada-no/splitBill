70ed2aaa6f5332c36bc3b34a4f6b2177bc4af0cf
nada <nada@nada.nada>
2022-08-18 17:20:30 +0200

    math solved, added expenses list view

--- splitBill.js
+++ splitBill.js
@@ -3,21 +3,35 @@ var sb = {
     addExpVw: null,
     resultsVw: null,
     homeVw: null,
+    expListVw: null,
+    closeExpVw: null,
+    closeExpListVw: null,
+    expList: null,
     conceptInp: null,
     amountInp: null,
     errorInp: null,
     addExpBtn: null,
     toAddExp: null,
-    closeExpVw: null,
+    toExpList: null,
     results: null,
+    debts: [],
+    expenses: [],
+
 
     //setup the app
     init: () => {
         sb.homeVw = document.getElementById("home");
         sb.resultsVw = document.getElementById("resultsView");
         sb.addExpVw = document.getElementById("addExpenseView");
+        sb.expListVw = document.getElementById("allExpensesView");
+        sb.expList = document.getElementById("expensesList");
         sb.resultsVw.classList.add("hidden");
         sb.addExpVw.classList.add("hidden");
+        sb.expListVw.classList.add("hidden");
+        sb.closeExpVw = document.getElementById("closeExpVw");
+        sb.closeExpVw.onclick = sb.list;
+        sb.closeExpListVw = document.getElementById("closeExpListVw");
+        sb.closeExpListVw.onclick = sb.list;
         sb.amountInp = document.getElementById("amount");
         sb.conceptInp = document.getElementById("concept");
         sb.errorInp = document.getElementById("errorInp");
@@ -26,20 +40,31 @@ var sb = {
         sb.addExpBtn.onclick = sb.addExp;
         sb.toAddExp = document.getElementById("toAddExp");
         sb.toAddExp.onclick = sb.openAddExp;
-        sb.closeExpVw = document.getElementById("closeExpVw");
-        sb.closeExpVw.onclick = sb.list;
+        sb.toExpList = document.getElementById("toExpList");
+        sb.toExpList.onclick = sb.openExpList;
         sb.results = document.getElementById("results");
 
         //UPDATE LISTENER new users & expenses
         window.webxdc.setUpdateListener(function (update) {
             if (update.payload.type === "addUser") {
-                sb.participants.push({ name: update.payload.name, debts: [] });
+                sb.participants.push({ name: update.payload.name, debts: {}, paid: 0 });
             } else if (update.payload.type === "expense") {
+                //store the expense
+                sb.expenses.push({
+                    concept: update.payload.concept,
+                    amount: update.payload.amount,
+                    participants: update.payload.payees
+                });
                 //divide the expense between the people
-                    let amountPP = update.payload.amount / sb.participants.length;
-                    amountPP = Number.parseFloat(amountPP.toFixed(2));
-                sb.participants.forEach((person) => {
-                    if (person.name != update.payload.payer) person.debts.push({ payee: update.payload.payer, amount: amountPP, payers: update.payload.payers });
+                let amountPP = update.payload.amount / sb.participants.length;
+                amountPP = Number.parseFloat(amountPP.toFixed(2));
+                let paidFor = {};
+                update.payload.payees.forEach(element => {
+                    paidFor[element] = amountPP;
+                });
+                sb.debts.push({
+                    paidBy: update.payload.payer,
+                    paidFor: paidFor,
                 });
             }
             sb.list();
@@ -53,26 +78,31 @@ var sb = {
         //close the other views
         sb.hideAll();
         sb.resultsVw.classList.remove("hidden");
+
         //list debts
         sb.results.innerHTML = "";
         for (const person of sb.participants) {
             let labelContainer = document.createElement("div");
             let label = document.createElement("p");
+            let debtsUl = document.createElement("ul");
+
             let debt = 0;
-            //calculate the amount of money every person owes
-            person.debts.forEach(
-                (dbt) => {
-                    debt += dbt.amount;
+            let debts = simplifyDebts(sb.debts);
+
+            //CALCULATING DEBTS
+            debts.forEach(el => {
+                if (el[0] === person.name) {
+                    debt += el[2];
+                    let li = document.createElement("li");
+                    li.textContent = el[2].toFixed(2) + "€ to " + el[1];
+                    debtsUl.appendChild(li);
                 }
-            );
-            label.textContent = person.name + " owes " + debt + " in total.";
+            });
+
+            label.textContent = person.name + " owes " + debt.toFixed(2) + " in total.";
             labelContainer.appendChild(label);
-            labelContainer.setAttribute("id-data", person.name);
-            // labelContainer.addEventListener("click", (el) => {
-            //     sb.showDebts(el.currentTarget.getAttribute("id-data"));
-            // });
+            labelContainer.appendChild(debtsUl);
             sb.results.appendChild(labelContainer);
-            sb.showDebts(person.name);
         }
     },
 
@@ -110,7 +140,10 @@ var sb = {
             sb.errorInp.classList.add("hidden");
 
             let info = window.webxdc.selfName + " added a " + amount + "€ expense";
-            let payers = sb.participants.map(el => el.name);
+            let payees = [];
+            sb.participants.forEach(el => {
+                if (el.name !== window.webxdc.selfName) payees.push(el.name);
+            });
             //send an update
             window.webxdc.sendUpdate(
                 {
@@ -119,7 +152,7 @@ var sb = {
                         type: "expense",
                         concept: concept,
                         amount: amount,
-                        payers: payers,
+                        payees: payees,
                     },
                     info
                 },
@@ -127,32 +160,6 @@ var sb = {
             );
             sb.list();
         }
-    },
-
-    //show a user's debts
-    showDebts: (name) => {
-        //get the debts
-        let payees = [];
-        let totalDebt = 0;
-        let container = document.querySelector(`[id-data="${name}"]`);
-        let ul = document.createElement("ul");
-        const { debts } = sb.participants.find((el) => {
-            return el.name === name;
-        });
-
-        for (const debt of debts) {
-            if (!payees.includes(debt.payee)) payees.push(debt.payee);
-        }
-
-        for (const person of payees) {
-            debts.forEach((el) => {
-                if (el.payee === person) totalDebt += el.amount;
-            });
-            let payee = document.createElement("li");
-            payee.textContent = totalDebt + "€ to " + person;
-            ul.appendChild(payee);
-        }
-        container.appendChild(ul);
     },
 
     //open add expense screen
@@ -164,11 +171,33 @@ var sb = {
 
     },
 
+    //open and display exp list
+    openExpList: () => {
+        //display the view
+        sb.hideAll();
+        sb.expListVw.classList.remove("hidden");
+        //list the expenses
+        sb.expList.innerHTML = "";
+
+        if (sb.expenses.length == 0) {
+            let p = document.createElement("p");
+                p.textContent = "There's no expenses yet";
+                sb.expList.appendChild(p);
+        } else {
+            for (const exp of sb.expenses) {
+                let p = document.createElement("p");
+                p.textContent = exp.concept + " - " + exp.amount;
+                sb.expList.appendChild(p);
+            }
+        }
+    },
+
     //hide all views
     hideAll: () => {
         sb.homeVw.classList.add("hidden");
         sb.resultsVw.classList.add("hidden");
         sb.addExpVw.classList.add("hidden");
+        sb.expListVw.classList.add("hidden");
     },
 
 };
